3ef803e4c26ef767bca9da733ea9a551040bb7f9
Guy Castel <guycastel@gmail.com>
2025-04-23 10:07:49 +0300

    minor import tweak

--- client/src/types.ts
+++ client/src/types.ts
@@ -1,9 +1,10 @@
+import L from 'leaflet'
+
 /**
  * ISSNowResponseData type is used by server and client.
  * For the sake of simplicity of this small project this type is duplicated to both
  * places manually rather then configuring some magic or using a cumbersome library.
  * */
-
 export interface ISSNowResponseData {
 	message: string
 	timestamp: number
